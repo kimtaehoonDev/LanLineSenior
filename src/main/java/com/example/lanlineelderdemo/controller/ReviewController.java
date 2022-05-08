b9e2980be07d34ac89670ac4729cb098532642d6
iamtaehoon <kimth9981@naver.com>
2022-05-08 19:01:13 +0900

    feat(): 댓글 공백 저장되지 않게 검증 구현

--- src/main/java/com/example/lanlineelderdemo/controller/ReviewController.java
+++ src/main/java/com/example/lanlineelderdemo/controller/ReviewController.java
@@ -5,6 +5,7 @@ import com.example.lanlineelderdemo.service.review.CreateReviewRequestServiceDto
 import com.example.lanlineelderdemo.service.review.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,7 +17,7 @@ public class ReviewController {
 
     @PostMapping("/reviews")
     public String postReview(@RequestParam Long restaurantId,
-                             @ModelAttribute CreateReviewRequestDto createReviewRequestDto) {
+                             @Validated @ModelAttribute CreateReviewRequestDto createReviewRequestDto) {
         reviewService.leaveReview(new CreateReviewRequestServiceDto(restaurantId, createReviewRequestDto.getContent(),
                 createReviewRequestDto.getName(), createReviewRequestDto.getPassword()));
         return "redirect:/restaurants/"+restaurantId;
--- src/main/java/com/example/lanlineelderdemo/controller/dto/CreateReviewRequestDto.java
+++ src/main/java/com/example/lanlineelderdemo/controller/dto/CreateReviewRequestDto.java
@@ -1,12 +1,18 @@
 package com.example.lanlineelderdemo.controller.dto;
 
-import com.example.lanlineelderdemo.service.review.CreateReviewRequestServiceDto;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
 
 @Data
 public class CreateReviewRequestDto {
+    @NotBlank
     private String name;
+    @NotBlank
     private String password;
+    @NotBlank
     private String content;
 
 }
